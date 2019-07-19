bc168f1e54efe125afff83a4c78a27fe369b8efb
László Horváth <bedafoltyn@gmail.com>
2019-07-19 13:07:27 +0200

    new states added

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const svg = document.querySelector('svg')
-const animateElements = Array.from(document.querySelectorAll('animate'))
+const insvestmentsElements = Array.from(document.querySelectorAll('.investment'))
+const librariesElements = Array.from(document.querySelectorAll('.library'))
 
-svg.addEventListener('click', function () {
-  animateElements.forEach(el => el.beginElement())
+const investmentButton = document.querySelector('.investment-button')
+
+investmentButton.addEventListener('click', function () {
+  insvestmentsElements.forEach(el => el.beginElement())
+})
+
+const libraryButton = document.querySelector('.library-button')
+
+libraryButton.addEventListener('click', function () {
+  librariesElements.forEach(el => el.beginElement())
 })
